c3b73d9bf30a3bf64ce33b19c3473c721d20c165
Alexander Panchenko <panchenko.alexander@gmail.com>
2018-03-11 11:00:42 -0700

    proxy print

--- proxy.py
+++ proxy.py
@@ -1,5 +1,10 @@
 from flask import Flask, request, Response
 import logging
+import requests
+
+
+endpoint = "http://localhost:8181/spotlight"
+
 
 app = Flask(__name__)
 
@@ -9,13 +14,17 @@ log = logging.getLogger("proxy.py")
 
 @app.route("/", methods=['POST'])
 def hello():
-    log.info(request.headers)
-    log.info(request.data)
+    h = {key: value for key, value in request.headers}
+    r = requests.post(endpoint, headers=h, data=request.data)
 
+    resp = Response()
+    if r.status_code == 200:
+        for header_name, header_value in r.headers.items():
+            resp.headers[header_name] = header_value
+        resp.data = r.content
+    else:
+        print("Warning: server returned an error")
+        print(r)
 
-    # add here the original headers
-    resp = Response("Foo bar baz")
-    resp.headers['Access-Control-Allow-Origin'] = '*'
-    resp.data = request.data
     return resp
 
