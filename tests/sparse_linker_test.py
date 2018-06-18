f5c73d6674265eac0fd5efe9f6555a81e033046d
Alexander Panchenko <panchenko.alexander@gmail.com>
2018-06-18 07:45:45 -0700

    add the cache directory

--- tests/sparse_linker_test.py
+++ tests/sparse_linker_test.py
@@ -6,7 +6,7 @@ dataset_fpaths = ["../datasets/dbpedia.tsv",
                   "../datasets/kore50.tsv",
                   "../datasets/n3-reuters-128.tsv"]
 
-sl = SparseLinker("../data/test99_new")
+sl = SparseLinker("../data/sparse_model")
 sl.train(dataset_fpaths)
 context = "Madonna is a great music  signer and lives near West Holywood in LA. adonna Louise Ciccone (/tʃɪˈkoʊni/; born August 16, 1958) is an American singer, songwriter, actress, and businesswoman. Referred to as the Queen of Pop since the 1980s, Madonna is known for pushing the boundaries of lyrical content in mainstream popular music, as well as visual imagery in music videos and on stage. She has also frequently reinvented both her music and image while maintaining autonomy within the recording industry. Besides sparking controversy, her works have bee "
 phrases = ["Madonna"]
