e95265925808c8175c509d095e226d4d84b92592
Özge Sevgili <ozgeesevgili@gmail.com>
2018-12-19 22:28:32 +0100

    Create fully connected neural network.

---
+++ supervised/nn.py
@@ -0,0 +1,269 @@
+import tensorflow as tf
+from gensim.models import KeyedVectors, Doc2Vec
+import codecs
+from sqlitedict import SqliteDict
+from nltk.tokenize import RegexpTokenizer
+import numpy as np
+from random import shuffle
+import argparse
+from tensorflow.contrib.tensorboard.plugins import projector
+
+
+def create_input_from_sample_efficient(path, graph_embeds, doc2vec, lookupdb, longabsdb):
+    samples = list()
+    tokenizer = RegexpTokenizer(r'\w+')
+
+    with codecs.open(path, "r", "utf-8") as sample:
+        line = sample.readline()
+        while line != "":
+            splitted = line.split('\t')
+            line = sample.readline()
+            try:
+                entity, beg, end, true_url, context, negative_samples_urls = splitted[0], int(splitted[1]), int(splitted[2]), \
+                                                                    splitted[3], splitted[4], splitted[5:]
+            except:
+                continue
+
+            try:
+                wordvec = doc2vec[entity]
+            except KeyError:
+                wordvec = doc2vec.infer_vector(entity)
+
+            contextvec = doc2vec.infer_vector(tokenizer.tokenize(context))
+
+            try:
+                longab = longabsdb[true_url]
+            except KeyError:
+                continue
+
+            try:
+                graph_embed = graph_embeds[str(lookupdb[true_url])]
+            except KeyError:
+                continue
+            longabvec = doc2vec.infer_vector(tokenizer.tokenize(longab))
+
+            inputvec = np.concatenate((np.array(wordvec), np.array(graph_embed),
+                                       np.array(contextvec), np.array(longabvec)), axis=0)
+            # 1 means positive
+            samples.append((inputvec, np.array([1])))
+
+            for negative_url in negative_samples_urls:
+                negative_url = negative_url.strip()
+                try:
+                    longab = longabsdb[negative_url]
+                except KeyError:
+                    continue
+
+                try:
+                    graph_embed = graph_embeds[str(lookupdb[negative_url])]
+                except KeyError:
+                    continue
+
+                longabvec = doc2vec.infer_vector(tokenizer.tokenize(longab))
+
+                inputvec = np.concatenate((np.array(wordvec), np.array(graph_embed),
+                                           np.array(contextvec), np.array(longabvec)), axis=0)
+                # 0 negative
+                samples.append((inputvec, np.array([0])))
+
+    return samples
+
+
+def format(list_sample):
+    inputs, outputs = list(), list()
+    index = 0
+
+    for sample in list_sample:
+        inputs.insert(index, sample[0])
+        outputs.insert(index, sample[1])
+        index += 1
+
+    return np.array(inputs), np.array(outputs)
+
+
+def prepare_training_test_set_efficient(path_samples, graph_embeds, doc2vec, lookupdb, longabsdb):
+    samples = create_input_from_sample_efficient(path_samples, graph_embeds, doc2vec, lookupdb, longabsdb)
+
+    shuffle(samples)
+
+    training_size = int(len(samples) * 0.8)
+    training_set = samples[:training_size]
+    test_set = samples[training_size:]
+    del samples
+
+    training_inputs, training_outputs = format(training_set)
+    del training_set
+    test_inputs, test_outputs = format(test_set)
+    del test_set
+    return training_inputs, training_outputs, test_inputs, test_outputs
+
+
+def load_embeds(path_graphembed, path_doc2vec):
+    graph_embeds = KeyedVectors.load_word2vec_format(path_graphembed, binary=False)
+    print('graph_embeds are loaded')
+
+    doc2vec = Doc2Vec.load(path_doc2vec, mmap = 'r')
+    print('doc2vec is loaded')
+
+    return graph_embeds, doc2vec
+
+
+def load_db(path_lookupdb, path_longabsdb):
+
+    lookupdb = SqliteDict(path_lookupdb, autocommit=False)
+    print('lookup is loaded')
+
+    longabsdb = SqliteDict(path_longabsdb, autocommit=False)
+    print('long abstract is loaded')
+
+    return lookupdb, longabsdb
+
+
+def get_parameters():
+    parser = argparse.ArgumentParser(description='Performs training to discriminate the correct disambiguated entity '
+                                                 'for an ambigous mention of an entity using '
+                                                 'graph embeddings and doc2vec.')
+
+    parser.add_argument('-training_corpus',
+                        help="Path to a training corpus contaning both positive and negative examples"
+                             " in the format -- positive sample, starting location of the ambigous word, "
+                             "endinglocation of the ambigous word , true_url, context, negative_samples_urls -- "
+                             "seperated by the tabs in text form.",
+                        default='/Users/sevgili/Ozge-PhD/DBpedia-datasets/training-datasets/csv/negative_samples_filtered_randomly_10.tsv')
+
+    parser.add_argument('-graph_embedding', help="Path for the pretrained embeddings of a graph "
+                                                 "there is an embedding for each node in the entity graph.",
+                        default='/Users/sevgili/Ozge-PhD/DBpedia-datasets/outputs/nodes.embeddings')
+
+    parser.add_argument('-doc2vec', help="Path for pretrained doc2vec.",
+                        default='/Users/sevgili/Ozge-PhD/wikipedia-doc2vec/all-dim100/wikipedia_document_dim100_with_wikicorpus.doc2vec')
+
+    parser.add_argument('-lookup_db',
+                        help="Path for the nodes lookup database which is SqliteDict and whose keys denote the "
+                             "entities in the graph and values refer to the id of each entity.",
+                        default='/Users/sevgili/Ozge-PhD/DBpedia-datasets/outputs/databases/intersection_nodes_lookup.db')
+
+    parser.add_argument('-long_abstracts_db', help="Path for the nodes long abstracts database which is SqliteDict "
+                                                   "and whose keys denote the node id "
+                                                   "and values contain the long abstracts of each entity.",
+                        default='/Users/sevgili/Ozge-PhD/DBpedia-datasets/outputs/databases/long_abstracts.db')
+
+    parser.add_argument('-learning_rate', help="Learning rate for the optimizer in neural network (default 0.00005).",
+                        default=0.005, type=float)
+
+    parser.add_argument('-training_epochs', help="The number of epochs to train the neural network (default 900).",
+                        default=150000, type=int)
+
+    parser.add_argument('-h1_size', help="The size of the first hidden layer the neural network (default 100).",
+                        default=100, type=int)
+
+    parser.add_argument('-h2_size', help="The size of the second hidden layer the neural network (default 100).",
+                        default=100, type=int)
+
+    args = parser.parse_args()
+
+    return args.training_corpus, args.graph_embedding, args.doc2vec, args.lookup_db, args.long_abstracts_db, \
+           args.learning_rate, args.training_epochs, args.h1_size, args.h2_size
+
+
+if __name__ == "__main__":
+    training_corpus, path_graphembed, path_doc2vec, path_lookupdb, path_longabsdb, \
+    learning_rate, training_epochs, h1_size, h2_size = get_parameters()
+
+    graph_embeds, doc2vec = load_embeds(path_graphembed, path_doc2vec)
+    lookupdb, longabsdb = load_db(path_lookupdb, path_longabsdb)
+
+    inputs, outputs, test_inputs, test_outputs = prepare_training_test_set_efficient(training_corpus, graph_embeds, doc2vec, lookupdb, longabsdb)
+
+    print(inputs.shape, outputs.shape)
+
+    # Layer's sizes
+    x_size = inputs.shape[1]
+    y_size = outputs.shape[1]
+
+    # Symbols
+    X = tf.placeholder(tf.float32, shape=[None, x_size])
+    y = tf.placeholder(tf.float32, shape=[None, y_size])
+
+    # Weight initializations
+    weights = {
+        'w1': tf.Variable(tf.random_normal([x_size, h1_size])),
+        'w2': tf.Variable(tf.random_normal([h1_size, h2_size])),
+        'out': tf.Variable(tf.random_normal([h2_size, y_size]))
+    }
+
+    biases = {
+        'b1': tf.Variable(tf.random_normal([h1_size])),
+        'b2': tf.Variable(tf.random_normal([h2_size])),
+        'out': tf.Variable(tf.random_normal([y_size]))
+    }
+
+
+    # Forward propagation
+    h1 = tf.add(tf.matmul(X, weights['w1']), biases['b1'])
+    h2 = tf.add(tf.matmul(h1, weights['w2']), biases['b2'])
+    yhat = tf.add(tf.matmul(h2, weights['out']), biases['out'])
+
+    # Backward propagation
+    loss = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(logits=yhat, targets=y))
+    optimizer = tf.train.GradientDescentOptimizer(learning_rate).minimize(loss)
+
+    # -- metrics --
+    # Accuracy
+    pred = tf.nn.sigmoid(yhat)
+    correct_prediction = tf.equal(tf.round(pred), y)
+    accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+
+    # Precision, Recall, F1
+
+    TP = tf.count_nonzero(tf.round(pred) * y)
+    TN = tf.count_nonzero((tf.round(pred) - 1) * (y - 1))
+    FP = tf.count_nonzero(tf.round(pred) * (y - 1))
+    FN = tf.count_nonzero((tf.round(pred) - 1) * y)
+
+    precision = TP / (TP + FP)
+    recall = TP / (TP + FN)
+    f1 = 2 * precision * recall / (precision + recall)
+
+    with tf.device('/cpu'):
+        with tf.Session() as sess:
+            loss_summary = tf.summary.scalar('loss', loss)
+            accuracy_summary = tf.summary.scalar('accuracy', accuracy)
+            precision_summary = tf.summary.scalar('precision', precision)
+            recall_summary = tf.summary.scalar('recall', recall)
+            f1_summary = tf.summary.scalar('f1_summary', f1)
+
+            config = projector.ProjectorConfig()
+
+            train_summary_op = tf.summary.merge_all()
+            #summary_writer = tf.summary.FileWriter('nn.log', sess.graph)
+            #projector.visualize_embeddings(summary_writer, config)
+
+            sess.run(tf.global_variables_initializer())
+
+            for epoch in range(training_epochs):
+                _, current_loss = sess.run([optimizer, loss], feed_dict={X: inputs, y: outputs})
+
+                if epoch % 100==0 and epoch != 0:
+                    print(current_loss)
+                    print("Training Accuracy:", accuracy.eval({X: inputs, y: outputs}))
+                    print("Precision:", precision.eval({X: inputs, y: outputs}),
+                          "Recall:", recall.eval({X: inputs, y: outputs}), "F1:", f1.eval({X: inputs, y: outputs}))
+                    summary_str = sess.run(train_summary_op, feed_dict={X: inputs, y: outputs})
+                    #summary_writer.add_summary(summary_str, epoch)
+
+            # Test model
+            pred = tf.nn.sigmoid(yhat)
+            correct_prediction = tf.equal(tf.round(pred), y)
+
+            accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+            print("Training Accuracy:", accuracy.eval({X: inputs, y: outputs}))
+            print("Training Precision:", precision.eval({X: inputs, y: outputs}),
+                  "Training Recall:", recall.eval({X: inputs, y: outputs}), " Training F1:", f1.eval({X: inputs, y: outputs}))
+
+            print("Test Accuracy:", accuracy.eval({X: test_inputs, y: test_outputs}))
+            print("Test Precision:", precision.eval({X: test_inputs, y: test_outputs}),
+                  "Test Recall:", recall.eval({X: test_inputs, y: test_outputs}), " Test F1:",
+                  f1.eval({X: test_inputs, y: test_outputs}))
+
+
